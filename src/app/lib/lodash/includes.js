fa7b235559d11a11dacd0ec8af7202df85bc0471
Chloe <2592538673@qq.com>
2019-06-17 17:10:36 +0800

    完善 includes ,适用 array, object, string

--- src/app/lib/lodash/includes.js
+++ src/app/lib/lodash/includes.js
@@ -1,17 +1,35 @@
 import isArray from './isArray'
+import isObject from './isObject'
+import isString from './isString'
 
-function includes(arr, item) {
-  if (isArray(arr)) {
-    let hasItem = false
-    for (let i = 0; i < arr.length; i++) {
-      if (item === arr[i]) {
+function includes(data, item, index) {
+  let hasItem = false
+  if (isArray(data)) {
+    if (!!index && data[index] === item) {
+      return (hasItem = true)
+    } else if (!!index && data[index] !== item) {
+      return hasItem
+    } else {
+      for (let i = 0; i < data.length; i++) {
+        if (item === data[i]) {
+          return (hasItem = true)
+        } else {
+          hasItem = false
+        }
+      }
+    }
+  } else if (isObject(data)) {
+    for (let i in data) {
+      if (item === data[i]) {
         return (hasItem = true)
       } else {
         hasItem = false
       }
     }
-    return hasItem
+  } else if (isString(data)) {
+    hasItem = !!(data.indexOf(item) !== -1)
   }
+  return hasItem
 }
 
 export default includes
